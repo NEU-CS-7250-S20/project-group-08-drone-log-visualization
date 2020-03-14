6a46df43f116d21909498244100c74ac3d9aacd0
Ondrej Biza <ondrej.biza@gmail.com>
2020-03-14 15:20:19 -0400

    load telemetry data

---
+++ js/load_data.js
@@ -0,0 +1,71 @@
+function parseFloatWithCommaDecimal(string) {
+    // parse a float with a comma instead of a dot for the decimal
+    let tmp = string.replace(",", ".");
+    // TODO: deal with NANs somewhere, or assume the log is always valid
+    return parseFloat(tmp);
+}
+
+function parseT02(d) {
+    // parse a single line of telemetry 2
+    return {
+        time: parseFloatWithCommaDecimal(d.time),
+        altitude: parseFloatWithCommaDecimal(d.altitude),
+        posLat: parseFloatWithCommaDecimal(d.posLat),
+        posLon: parseFloatWithCommaDecimal(d.posLon),
+        psi: parseFloatWithCommaDecimal(d.psi),
+        theta: parseFloatWithCommaDecimal(d.theta),
+        phi: parseFloatWithCommaDecimal(d.phi),
+        R: parseFloatWithCommaDecimal(d.R),
+        Q: parseFloatWithCommaDecimal(d.Q),
+        P: parseFloatWithCommaDecimal(d.P),
+        accZ: parseFloatWithCommaDecimal(d.accZ),
+        accY: parseFloatWithCommaDecimal(d.accY),
+        accX: parseFloatWithCommaDecimal(d.accX),
+        windHdg: parseFloatWithCommaDecimal(d.windHdg),
+        windSpeed: parseFloatWithCommaDecimal(d.windSpeed),
+        airSpeed: parseFloatWithCommaDecimal(d.airSpeed),
+        groundSpeed: parseFloatWithCommaDecimal(d.groundSpeed),
+        track: parseFloatWithCommaDecimal(d.track),
+        amslCorr: parseFloatWithCommaDecimal(d.amslCorr),
+        TAS: parseFloatWithCommaDecimal(d.TAS),
+        OAT: parseFloatWithCommaDecimal(d.OAT)
+    };
+}
+
+function parseT15(d) {
+    // parse a single line of telemetry 15
+    return {
+        time: parseFloatWithCommaDecimal(d.time),
+        uUpper: parseInt(d.uUpper),
+        iUpper: parseInt(d.iUpper),
+        gpsErr: parseInt(d.gpsErr),
+        sensorErr: parseInt(d.sensorErr),
+        uLower: parseInt(d.uLower),
+        iLower: parseInt(d.iLower),
+        qLower: parseInt(d.qLower),
+        lastLink: parseInt(d.lastLink),
+        timeLeft: parseInt(d.timeLeft),
+        userTimeLeft: parseInt(d.userTimeLeft),
+        yy: parseInt(d.yy),
+        MM: parseInt(d.MM),
+        dd: parseInt(d.dd),
+        hh: parseInt(d.hh),
+        mm: parseInt(d.mm),
+        ss: parseInt(d.ss),
+        msec: parseInt(d.msec),
+        flags: parseInt(d.flags)
+    };
+}
+
+function parseT16(d) {
+    // parse a single line of telemetry 16
+    return {
+        time: parseFloatWithCommaDecimal(d.time),
+        flags: parseInt(d.flags),
+        procId: parseInt(d.procId),
+        fpStatus: parseInt(d.fpStatus),
+        refAs: parseInt(d.refAs),
+        refAlt: parseInt(d.refAlt),
+        fpItemId: parseInt(d.fpItemId)
+    }
+}
--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,15 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
 ((() => {
 
-  console.log("Hello, world!");
+    const delimiter = ";";
+
+    // load telemetry data
+    // https://stackoverflow.com/questions/21842384/importing-data-from-multiple-csv-files-in-d3
+    Promise.all([
+        d3.dsv(delimiter, "data/sn11401003-2018-09-11-14-13-ses-00.T02.csv", parseT02),
+        d3.dsv(delimiter, "data/sn11401003-2018-09-11-14-13-ses-00.T15.csv", parseT15),
+        d3.dsv(delimiter, "data/sn11401003-2018-09-11-14-13-ses-00.T16.csv", parseT16),
+    ]).then(function(files) {
+        console.log(files);
+    });
 
 })());
