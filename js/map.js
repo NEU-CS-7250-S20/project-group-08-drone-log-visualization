d41a1e56f8294bdb0642a0aa278994484dac2d9f
Ondrej Biza <ondrej.biza@gmail.com>
2020-03-22 22:07:30 -0400

    refactored map and added acks

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
 // slider: https://bl.ocks.org/johnwalley/e1d256b81e51da68f7feb632a53c3518
+// https://developers.google.com/maps/documentation/javascript/examples/polyline-simple
+// https://developers.google.com/maps/documentation/javascript/examples/polyline-remove
 
 function mapplot() {
 
     let width = 1.0, height = 1.0, maxPoints = 100;
 
-    function chart(selector, data) {
+    function chart(mapSelector, sliderSelector, data) {
 
         // get the average longitude and latitude; this will be our starting point for the map *for now*
         let t02 = data;
@@ -18,73 +20,77 @@ function mapplot() {
         let avgLat = (lat.reduce((prev, cur) => cur += prev)) / lat.length;
         let avgLon = (lon.reduce((prev, cur) => cur += prev)) / lon.length;
 
-        d3.select(selector).attr("style", "width: " + width * 100 + "%; height: " + height * 100 + "%; float: left; display: inline-block;");
+        // modify map styles
+        d3.select(mapSelector)
+            .attr(
+                "style", "width: " + width * 100 +
+                "%; height: " + height * 100 +
+                "%; float: left; display: inline-block;"
+            );
 
+        // subsample data and setup google map
         let t02Subset = subsample(t02, maxPoints);
 
-        // setup google map
-        let map = new google.maps.Map(d3.select(selector).node(), {
-            zoom: 16,
+        let map = new google.maps.Map(d3.select(mapSelector).node(), {
+            zoom: 15,
             center: new google.maps.LatLng(avgLat, avgLon),
             mapTypeId: google.maps.MapTypeId.HYBRID
         });
 
-        var flightPlanCoordinates = objectsToGMapsCoordinates(t02Subset);
-
-        var flightPath = new google.maps.Polyline({
-          path: flightPlanCoordinates,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
-        });
-
-        flightPath.setMap(map);
+        // add path to the map
+        let coordinates = objectsToGMapsCoordinates(t02Subset);
+        let flightPath = addPath(coordinates, map);
 
         // setup slider
-        let sliderSimple = d3
-            .sliderBottom()
+        let slider = d3.sliderBottom()
             .min(0.0)
             .max(1.0)
             .width(300)
             .tickFormat(d3.format('.2'))
             .ticks(5)
-            .default(0.5)
+            .default(1.0)
             .on('onchange', function(val) {
-                // subsample GPS coordinates, we have trouble displaying more than 150 at the same time
-                // we will probably switch from Google Maps API to leaflet or something similar
+
                 let tmpNumPoints = Math.round(val * t02.length);
                 let tmpT02 = t02.slice(0, tmpNumPoints);
                 let tmpMaxPoints = Math.min(tmpNumPoints, maxPoints);
 
                 t02Subset = subsample(tmpT02, tmpMaxPoints);
+                coordinates = objectsToGMapsCoordinates(t02Subset);
 
-                flightPath.setMap(null);
+                removePath(flightPath);
+                flightPath = addPath(coordinates, map);
 
-                flightPlanCoordinates = objectsToGMapsCoordinates(t02Subset);
-
-                flightPath = new google.maps.Polyline({
-                  path: flightPlanCoordinates,
-                  geodesic: true,
-                  strokeColor: '#FF0000',
-                  strokeOpacity: 1.0,
-                  strokeWeight: 2
-                });
-
-                flightPath.setMap(map);
             });
 
-        /*
-        var gSimple = d3.select('div#slider-simple')
-            .append('svg')
-            .attr('width', 500)
-            .attr('height', 100)
-            .append('g')
-            .attr('transform', 'translate(30,30)');
+        let sliderGroup = d3.select(sliderSelector)
+            .append("svg")
+            .attr("width", 500)
+            .attr("height", 100)
+            .append("g")
+            .attr("transform", "translate(30,30)");
 
+        sliderGroup.call(slider);
 
-        gSimple.call(sliderSimple);
-        */
+    }
+
+    function addPath(coordinates, map) {
+
+        let flightPath = new google.maps.Polyline({
+            path: coordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        flightPath.setMap(map);
+        return flightPath
+
+    }
+
+    function removePath(flightPath) {
+        flightPath.setMap(null);
     }
 
     chart.width = function(_) {
